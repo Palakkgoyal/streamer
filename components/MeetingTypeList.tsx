c6a85ac916519a503f6170d459d2924d7c8de531
Palak Goyal <Palakgoyal0304@gmail.com>
2024-04-01 18:16:46 +0000

    add streamer and fix next allowed origin issue

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,14 +3,40 @@ import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { useStreamVideoClient } from "@stream-io/video-react-sdk";
 
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    title: "",
+    description: "",
+    link: "",
+  });
 
-  const createMeeting = () => {};
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+
+  const createMeeting = async () => {
+    if (!user || !client) return;
+
+    try {
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+
+      if (!call) throw new Error("Failed to create call");
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || "Instant Meeting";
+      await call.getOrCreate
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["localhost:3000", "https://bookish-space-guacamole-x6wvwpqw6w6fvrr4-3000.app.github.dev/"]
+        }
+    }
+};
 
 export default nextConfig;
